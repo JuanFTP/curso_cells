629c9c49bb3889ef54bc9a79ddf995147bc05d8d
Christian Alono <alonaranda@gmail.com>
2019-12-09 17:11:00 -0600

    JS-Teoria

---
+++ JS_Ejemplo1/JS/main.js
@@ -0,0 +1,59 @@
+let numbers = [45, 4, 9, 16, 25];
+let mayor18 = numbers.filter((value, index, array) => {
+    return value > 18
+});
+console.log(mayor18);
+console.log("------------------");
+
+let txt = '';
+let numeros = [45, 4, 9, 16, 25];
+numeros.forEach((value, index,array) => {
+    txt += value + '|';
+});
+console.log(txt);
+console.log("------------------");
+
+
+let a = {
+    nombre: 'Christian',
+    edad: 24
+};
+console.log(a);
+console.log("------------------");
+
+let frutas =['Fresa','Sandia','Melon','Mango'];
+let opciones = frutas.map((value, index, array) =>{
+    return{value: index +1, label:value}
+});
+console.log(opciones);
+console.log("------------------");
+
+let valores = [45, 4, 9, 16, 25];
+let menor10 = numbers.some((value, index, array) =>{
+    return value < 10
+});
+console.log(menor10);
+console.log("------------------");
+
+let asentamiento = [
+    {cp:06030, asentamiento: 'Tabacalera'},
+    {cp:55010, asentamiento: 'El diamante'},
+    {cp:55070, asentamiento: 'Villas del Sol'},
+    {cp:06200, asentamiento:'Morelos'},
+    {cp:06350, asentamiento: 'Buenavista'},
+    {cp:55020, asentamiento: '10 de Abril'}];
+console.log(asentamiento);
+console.log("-----RESULTADO-----");
+
+let resultado = asentamiento.filter((value,index, array) => {
+    return value.cp < 50000
+})
+console.log(resultado)
+let arreglo = resultado.map((value, index, array) =>{
+    return{value: index +1, label:value.asentamiento}
+});
+console.log(arreglo)
+
+
+
+
