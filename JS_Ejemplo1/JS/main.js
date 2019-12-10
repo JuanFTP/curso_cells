719ef87d2aad36f3a25c721ecac11e4a74bad4e4
Christian Alono <alonaranda@gmail.com>
2019-12-10 10:08:50 -0600

    Encontrar elementos

--- JS_Ejemplo1/JS/main.js
+++ JS_Ejemplo1/JS/main.js
@@ -1,3 +1,6 @@
+    //Ejemplos de JavaScript
+
+//Filter
 let numbers = [45, 4, 9, 16, 25];
 let mayor18 = numbers.filter((value, index, array) => {
     return value > 18
@@ -5,6 +8,7 @@ let mayor18 = numbers.filter((value, index, array) => {
 console.log(mayor18);
 console.log("------------------");
 
+//foreEach
 let txt = '';
 let numeros = [45, 4, 9, 16, 25];
 numeros.forEach((value, index,array) => {
@@ -13,7 +17,7 @@ numeros.forEach((value, index,array) => {
 console.log(txt);
 console.log("------------------");
 
-
+//Hacer un JSON
 let a = {
     nombre: 'Christian',
     edad: 24
@@ -21,6 +25,8 @@ let a = {
 console.log(a);
 console.log("------------------");
 
+
+//Map
 let frutas =['Fresa','Sandia','Melon','Mango'];
 let opciones = frutas.map((value, index, array) =>{
     return{value: index +1, label:value}
@@ -28,6 +34,7 @@ let opciones = frutas.map((value, index, array) =>{
 console.log(opciones);
 console.log("------------------");
 
+//Some
 let valores = [45, 4, 9, 16, 25];
 let menor10 = numbers.some((value, index, array) =>{
     return value < 10
@@ -35,6 +42,8 @@ let menor10 = numbers.some((value, index, array) =>{
 console.log(menor10);
 console.log("------------------");
 
+
+//Ejercicio
 let asentamiento = [
     {cp:06030, asentamiento: 'Tabacalera'},
     {cp:55010, asentamiento: 'El diamante'},
@@ -55,7 +64,7 @@ console.log(arreglo)
 console.log("------------------");
 
 /*
-console.log("----CallBack---")
+//CallBack
 function funcion1(dato, callback){
     callback(dato);
 }
@@ -66,6 +75,7 @@ function1(2, function(){alert('Pasaron algunos segundos');});
 console.log("------------------");
 */
 
+//Función flecha
 /*La flecha ayuda a no escribir function()*/
 saludo = () =>{
     return "Hola Mundo";
@@ -73,12 +83,64 @@ saludo = () =>{
 console.log(saludo());
 console.log("------------------");
 
+//IIFE Pattern
 (function () {
-    var tmp = ...;
+    var a = 10;
+    alert
 }() );
+console.log("------------------");
+
+//Closueres
+function createIncrementor(start){
+    return function () {
+        start++;
+        return start;
+    }
+}
+var inc = createIncrementor(5);
+console.log(inc());
+console.log(inc());
+console.log("------------------");
 
 
 
+/*Para cargar primero toda la página y luego empezar a buscar dentro del head
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        let a = document.getElementById("div_id");
+        console.log(a);
+     });
+</script>
+*/
 
+//Encontrar elementos con id
+console.log("ById");
+document.getElementById("div_id");
+console.log(document.getElementById("div_id"));
+console.log("------------------");
 
+//Encontrar elementos por TagName
+console.log("ByTagName");
+document.getElementsByTagName("p");
+console.log(document.getElementsByTagName("p"));
+console.log("------------------");
 
+//Encontrar elementos por ClassName
+console.log("ClassName");
+document.getElementsByClassName("item");
+console.log(document.getElementsByClassName("item"));
+console.log("------------------");
+
+//Encontrar elementos por selector CSS
+
+//Encontrar un elemento con querySelector
+console.log("querySelector");
+document.querySelector("#ejemplo1 li");
+console.log(document.querySelector("#ejemplo1 li"));
+console.log("------------------");
+
+//Encontrar elementos con querySelectorAll
+console.log("querySelectorAll");
+document.querySelectorAll("#ejemplo1 li");
+console.log(document.querySelectorAll("#ejemplo1 li"));
+console.log("------------------");
