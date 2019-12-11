f3a0c229dba8a7471e5e25268563ee472f3ca940
Christian Alono <alonaranda@gmail.com>
2019-12-11 15:47:53 -0600

    PromesasMetodosHTTP

---
+++ Promesas/main.js
@@ -0,0 +1,16 @@
+//Las promesas son clases
+/*
+Las promesas se utilizan para la realización de tareas asincronas, como por
+ejemplo la obtención de respuesta a una peticion HTTP
+>>Puede tener 4 estados:
+Pendiente
+Cumplida
+Rechazada
+Arreglada 
+*/
+
+//Crear Prome
+//Ver apuntes
+
+//Metodos HTTP
+//Ver apuntes
