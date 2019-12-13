764758112fbd669fe035fc4e65f73ea8c42aa9b7
Christian Alono <alonaranda@gmail.com>
2019-12-13 16:28:36 -0600

    Ejemplo2-LitElement

--- Cells/Ejemplo01/main.js
+++ Cells/Ejemplo01/main.js
@@ -6,10 +6,34 @@
 import {LitElement, html} from 'lit-element';
 
 class BasicLitElement extends LitElement{
+    
+    static get properties(){
+        return{
+            basicBind: {type: String},
+            groceriesList: {type: Array},
+            isAdmin: {type:Boolean},
+        }
+    }
+
+    constructor(){
+        super();
+        this.basicBind = "Hola mundo";
+        this.groceriesList =[
+            'manzana',
+            'uvas',
+            'mangos',
+            'pollo'
+        ];
+        this.isAdmin = true;
+    }
+
     render(){
         return html`
-            <h1>Ejemplo 01</h1>
-            <p>A paragraph</p>
+            <p>${this.basicBind}</p>
+            <ul>${this.groceriesList.map(food => html`<li>${food}</li>`)}</ul>
+            <p>${this.isAdmin?
+            html`Soy mortal` : html`Soy inmortal`
+            }</p>
         `;
     }
 }
